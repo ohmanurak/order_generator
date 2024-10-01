d5f723e1fb983b8259865d3abd85e66f46c11d04
natchapol watthanawong <natchapol.w@arise.tech>
2024-10-01 21:29:42 +0700

    remove miniset

--- constants/constants.py
+++ constants/constants.py
@@ -3,7 +3,6 @@ EKKAMAI_FONT_PATH = "fonts/EkkamaiNew-Regular.ttf"
 SHEET_URL = "https://docs.google.com/spreadsheets/d/17l5TKof_ZlDisjIW7ZxN6no24UHKSDvL4EmhDjqyeMI/edit?usp=sharing"
 
 PRICE_DICT = {
-    "Mini set": 245,
     "Small set": 425,
     "Big set": 1234,
     "1cha pint": 309,
